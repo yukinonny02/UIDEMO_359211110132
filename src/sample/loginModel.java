f618d0270ffee198defea69f85421ee1354fc73c
359211110132 <yukinonny02@gmail.com>
2018-02-25 15:11:13 +0700

    loginModel

--- src/sample/loginModel.java
+++ src/sample/loginModel.java
@@ -3,9 +3,12 @@ package sample;
 import dbUtil.dbConnection;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class loginModel {
+
     Connection connection;
 
     public loginModel() {
@@ -17,8 +20,34 @@ public class loginModel {
         if (this.connection == null){
             System.exit(1);
         }
-    }//loginModel
-    public boolean isDatabaseConnection(){
+
+
+    }//loginModel1
+    public  boolean isDatabaseConnection(){
         return this.connection != null;
-    }
-}//clas
+    }//isDatabaseConnection
+
+    public boolean isLogin(String user, String pass) throws SQLException {
+        PreparedStatement pr = null;
+        ResultSet rs = null;
+
+        String sql = "select * from user where username = ? and password = ?";
+        try {
+            pr = this.connection.prepareStatement(sql);
+            pr.setString(1,user);
+            pr.setString(2,pass);
+            rs = pr.executeQuery();
+            if (rs.next()){
+                return true;
+            }
+            return false;
+        }catch (SQLException e){
+            e.printStackTrace();
+            return  false;
+        }finally {
+            pr.close();
+            rs.close();
+        }
+
+    }//isLogin
+}//class
