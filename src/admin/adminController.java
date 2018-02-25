0248f87e75df72141b4b51847f98862f659576af
359211110132 <yukinonny02@gmail.com>
2018-02-25 16:35:22 +0700

    adminController

--- src/admin/adminController.java
+++ src/admin/adminController.java
@@ -1,4 +1,126 @@
 package admin;
 
-public class adminController {
-}
+import com.jfoenix.controls.JFXButton;
+import dbUtil.dbConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+import javafx.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class adminController implements Initializable {
+
+    private dbConnection db;
+    private ObservableList<StudentData> data;
+
+    @FXML
+    private TableView<StudentData> studentTable;
+
+    @FXML
+    private TableColumn<StudentData, String> idcolum;
+
+    @FXML
+    private TableColumn<StudentData, String> firstnamecolum;
+
+    @FXML
+    private TableColumn<StudentData, String> lastnamecolum;
+
+    @FXML
+    private TableColumn<StudentData, String> emailcolum;
+
+    @FXML
+    private TableColumn<StudentData, String> dobcolum;
+
+    @FXML
+    private JFXButton btnLoad;
+
+    @FXML
+    private TextField searchTxt;
+
+
+    @Override
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+        this.db = new dbConnection();
+    }
+
+    @FXML
+    private void loadStudentData(ActionEvent event){
+        try {
+            Connection conn = dbConnection.getConnection();
+            this.data = FXCollections.observableArrayList();
+            //sql
+            String sql = "Select * from student";
+            ResultSet rs = conn.createStatement().executeQuery(sql);
+            while (rs.next()) {
+
+                this.data.add(new StudentData(rs.getString(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getString(5)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        //put Data to tableview
+        this.idcolum.setCellValueFactory(
+                new PropertyValueFactory<StudentData,String>("id"));
+        this.firstnamecolum.setCellValueFactory(
+                new PropertyValueFactory<StudentData,String>("firstName"));
+        this.lastnamecolum.setCellValueFactory(
+                new PropertyValueFactory<StudentData,String>("lastName"));
+        this.emailcolum.setCellValueFactory(
+                new PropertyValueFactory<StudentData,String>("email"));
+        this.dobcolum.setCellValueFactory(
+                new PropertyValueFactory<StudentData,String>("DOB"));
+        this.studentTable.setItems(null);
+        this.studentTable.setItems(this.data);
+
+//Filter Data in TableView
+        FilteredList<StudentData> filteredData =
+                new FilteredList<>(data, e -> true);
+        searchTxt.setOnKeyReleased(e -> {
+            searchTxt.textProperty().addListener(
+                    (observable, oldValue, newValue) -> {
+                        filteredData.setPredicate(StudentData -> {
+                            if (newValue == null || newValue.isEmpty()) {
+                                return true;
+                            }
+                            String lowerCaseFilter = newValue.toLowerCase();
+                            if (StudentData.getId().contains(newValue)) {
+                                return true;
+                            } else if
+                                    (StudentData.getFirstName().toLowerCase().contains(lowerCaseFilter)) {
+                                return true;
+                            } else if
+                                    (StudentData.getLastName().toLowerCase().contains(lowerCaseFilter)) {
+                                return true;
+                            }
+                            return false;
+                        });
+                    });
+            SortedList<StudentData> sortedData =
+                    new SortedList<>(filteredData);
+            sortedData.comparatorProperty().bind(
+                    studentTable.comparatorProperty());
+            studentTable.setItems(sortedData);
+
+        });
+
+    }
+}//Class
